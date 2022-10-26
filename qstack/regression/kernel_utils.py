27caca39389d239ad973b17b08daf61e557eef9a
Yannick Calvino Alonso <yannick.calvinoalonso@epfl.ch>
2022-10-26 10:57:38 +0200

    using default dictionnary values for gdict (from Ksenia - origin)

--- qstack/regression/kernel_utils.py
+++ qstack/regression/kernel_utils.py
@@ -6,7 +6,7 @@ import argparse
 
 class ParseKwargs(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
-                    setattr(namespace, self.dest, dict())
+                    setattr(namespace, self.dest, defaults.gdict))
                     for value in values:
                         key, value = value.split('=')
                         for t in [int, float]:
