30b919549fa54a7b1ab99c5cf3a2e6a0304a82b3
calvinoY <yannick.calvinoalonso@epfl.ch>
2024-04-25 13:47:46 +0200

    Fix test_spahm_b

--- tests/test_spahm_b.py
+++ tests/test_spahm_b.py
@@ -4,12 +4,13 @@ from qstack.spahm.rho import bond, utils
 from qstack import compound
 
 def test_water():
-    xyz_in = './data/H2O.xyz'
+    path = os.path.dirname(os.path.realpath(__file__))
+    xyz_in = path+'/data/H2O.xyz'
     mols = utils.load_mols([xyz_in], [0], [0], 'minao')
     dms = utils.mols_guess(mols, [xyz_in], 'LB', spin=[0])
     X = bond.bond(mols, dms, spin=[0])
     X = np.hstack(X) # merging alpha-beta components for spin unrestricted representation #TODO: should be included into function not in main
-    true_file = 'data/H2O_spahm_b.npy_alpha_beta.npy'
+    true_file = path+'/data/H2O_spahm_b.npy_alpha_beta.npy'
     X_true = np.load(true_file)
     assert(X_true.shape == X.shape)
     for Xa, Xa_true in zip(X, X_true):
